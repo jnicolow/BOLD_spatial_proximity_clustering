26546a1efd0145ef05fc0d534954aa4ba3c898b6
jcnicolowUH <jnicolow@hawaii.edu>
2024-09-28 09:46:08 -1000

    controlling for request quota being too much

--- bold/boldAPI.py
+++ bold/boldAPI.py
@@ -2,17 +2,41 @@ import os
 import pandas as pd
 import requests
 
+def species_from_species_namestxt():
+    with open(os.path.join('bold', 'bold_queries', 'species_names.txt')) as f: lines = f.readlines()
+    return(lines[0].split(','))
+
 def get_sequences(species):
-    print(species)
+    print(f'downloading: {species}')
     url = f'https://www.boldsystems.org/index.php/API_Public/sequence?taxon={species}'
     response = requests.get(url)
-    sequnceInfos = response.text.split('>')[1:]
-    sequenceDf = pd.DataFrame(columns=['Species', 'BOLDProcessId', 'Sequence'])
-    for sequenceInfo in sequnceInfos:
-        dataSplit = sequenceInfo.split('|') 
-        species = dataSplit[1]
-        ID = dataSplit[0]
+
+    sequence_df = pd.DataFrame(columns=['species', 'BOLD_process_id', 'country', 'sequence'])
+    if 'exceeded your allowed request quota' in response.text:
+        print(response.text) # if you request to much they will say this
+        return sequence_df
+    sequence_infos = response.text.split('>')[1:] # the first element is just an xml header
+    
+    for sequence_info in sequence_infos:
+        data_split = sequence_info.split('|') 
+        species = data_split[1]
+        ID = data_split[0]
+        country = get_country(ID)
         # marker = dataSplit[2] # sometimes after the marker there is no | to solit on so in that case the markjer will have the sequence appended to it
-        sequenceRaw = dataSplit[-1] # some times the marker is missing so best to just take the last thing
-        sequenceDf = pd.concat([sequenceDf, pd.DataFrame({'Species':[species], 'BOLDProcessId':[ID], 'Sequence':[sequenceRaw.split('\n')[1].strip('\r')]})])
-    return sequenceDf
+        sequence_raw = data_split[-1] # some times the marker is missing so best to just take the last thing
+        sequence_df = pd.concat([sequence_df, pd.DataFrame({'species':[species], 'BOLD_process_id':[ID], 'country':[country], 'sequence':[sequence_raw.split('\n')[1].strip('\r')]})])
+    return sequence_df
+
+
+def get_country(process_id):
+    # return country from specimen api
+    url = f'https://www.boldsystems.org/index.php/API_Public/specimen?ids={process_id}'
+
+
+    response = requests.get(url)
+
+    country_string = '</country'
+    filtered_list = [item for item in response.text.split('>')[1:] if country_string in item] # [1:] because the first is always xml header
+    country = filtered_list[0].replace(country_string, '')
+    if country == '': country = 'Unknown'
+    return(country)
